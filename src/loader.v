7a5df75f5ac9c3e70f69dda3816399589e0d8474
Lenny Khazan <lenny@gochromatic.com>
2024-04-08 19:46:44 -0400

    make it a wire?

--- src/loader.v
+++ src/loader.v
@@ -1,6 +1,6 @@
 module ans_loader (
   input wire [`CNT_WIDTH-1:0] in,
-  output reg [`CNT_WIDTH-1:0] counts [`SYM_COUNT-1:0],
+  output wire [`CNT_WIDTH-1:0] counts [`SYM_COUNT-1:0],
 
   input wire in_vld,
   output reg in_rdy,
@@ -9,17 +9,18 @@ module ans_loader (
   input wire rst_n
 );
 
+reg [`CNT_WIDTH-1:0] counts_reg[`SYM_COUNT-1:0];
 reg [`SYM_WIDTH-1:0] counter;
 
 always @(posedge clk or negedge rst_n) begin
   if (!rst_n) begin
     in_rdy <= 1'b1;
     for (integer i = 0; i < `SYM_COUNT; i = i + 1) begin
-      counts[i] <= 0;
+      counts_reg[i] <= 0;
     end
     counter <= 0;
   end else if (in_rdy && in_vld) begin
-    counts[counter] <= in;
+    counts_reg[counter] <= in;
     counter <= counter + 1'b1;
     in_rdy <= 1'b0;
   end else if (!in_rdy && !in_vld) begin
