55aab0fcae6c8280683ed4a0482b6d892ad5401b
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2024-04-13 23:34:22 -0400

    [ANS] encoder test passing

--- src/encoder.v
+++ src/encoder.v
@@ -1,14 +1,14 @@
 module ans_encoder (
 
     input wire [`CNT_WIDTH-1:0] s_count,
-    input wire [`STATE_WIDTH-1:0] s_cumulative,
+    input wire [`SYM_WIDTH + `CNT_WIDTH - 1:0] s_cumulative,
     input wire [`STATE_WIDTH-1:0] total_count,
-    input  wire in_vld,
-    output reg  in_rdy,
+    input wire in_vld,
+    output reg in_rdy,
 
-    output reg  [`SYM_WIDTH-1:0] out,
-    output reg  out_vld,
-    input  wire out_rdy,
+    output reg [`SYM_WIDTH-1:0] out,
+    output reg out_vld,
+    input wire out_rdy,
 
     input wire clk,
     input wire ena,
@@ -28,15 +28,15 @@ module ans_encoder (
 
       if (state_reg >= ((1 << `SYM_WIDTH) * s_count)) begin
         out <= state_reg[`SYM_WIDTH-1:0];
-        state_reg <= (state_reg >> 4);
+        state_reg <= (state_reg >> `SYM_WIDTH);
         in_rdy <= 1'b0;
         out_vld <= 1'b1;
       end else begin
         state_reg = ((state_reg / s_count) * total_count) + s_cumulative + (state_reg % s_count);
-        in_rdy <= 1'b1;
+        in_rdy  <= 1'b1;
         out_vld <= 1'b0;
       end
-      
+
     end else if (ena && out_vld && out_rdy) begin
       // Output data has been read, go back to reading an input.
       out_vld <= 1'b0;
--- test/encoder/test.py
+++ test/encoder/test.py
@@ -3,6 +3,7 @@
 
 import sys
 from random import randint
+
 sys.path.append("..")
 
 import cocotb
@@ -11,23 +12,25 @@ from cocotb.triggers import ClockCycles
 
 import model.ans as ans
 
+
 def setup_model(data):
     model = ans.AnsHardware(alphabet_size=16, shift=4)
     for symbol in set(data):
         model.load_count(symbol, data.count(symbol))
     return model
 
+
 @cocotb.test()
 async def test_encoder_reset(dut):
-    
+
     dut._log.info("Start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-    
+
     # Setup the data model
     data_in = [x for x in range(16)]
     model = setup_model(data_in)
-    
+
     # Reset model
     model.reset()
 
@@ -53,21 +56,20 @@ async def test_encoder_state(dut):
     cocotb.start_soon(clock.start())
 
     # Setup the data model
-    data_in = [randint(1, 15) for x in range(16)]
+    data_in = [randint(0, 15) for x in range(16)]
     model = setup_model(data_in)
-    
+
     # Reset model
     model.reset()
     await test_encoder_reset(dut)
 
     for symbol in data_in:
-        
         output = model.encode(symbol)
 
         dut.s_count.value = model.counts[symbol]
         dut.s_cumulative.value = model.cumulative[symbol]
         dut.total_count.value = model.total_count
-        
+
         dut.in_vld.value = 1
         await ClockCycles(dut.clk, 1)
         dut.in_vld.value = 0
@@ -78,62 +80,7 @@ async def test_encoder_state(dut):
             await ClockCycles(dut.clk, 2)
             assert dut.out_vld.value == 0
             assert dut.out_reg.value == output
+        else:
+            assert output == None
 
         assert dut.encoder.state_reg.value == model.state
-
-
-
-
-
-
-
-
-
-
-# @cocotb.test()
-# async def test_encode_reset(dut):
-#     dut._log.info("Start")
-#     model = ans.AnsHardware(alphabet_size=16, shift=4)
-#     data_in = [x for x in range(16)]
-#     data_out = []
-#     for x in set(data_in):
-#         model.load_count(x, data_in.count(x))
-
-#     clock = Clock(dut.clk, 10, units="us")
-#     cocotb.start_soon(clock.start())
-
-#     # Reset
-#     dut._log.info("Reset")
-#     dut.ena.value = 1
-#     dut.ui_in.value = 0
-#     dut.uio_in.value = 0
-#     dut.rst_n.value = 0
-#     await ClockCycles(dut.clk, 10)
-#     dut.rst_n.value = 1
-
-#     model.reset_state()
-#     state = model.state
-#     state_reg = dut.user_project.ans_block.encoder.state_reg.value
-#     assert state_reg == state
-
-#     # Set the input values, wait one clock cycle, and check the output
-#     dut._log.info("Test")
-#     for d in data_in:
-#         out = model.encode(d)
-
-#         dut.ui_in.value = d
-#         dut.uio_in.value = 0b0101  # mode = encode, in_vld = 1, out_rdy = 0
-#         await ClockCycles(dut.clk, 10)
-
-#         state_reg = dut.user_project.ans_block.encoder.state_reg.value
-#         output_reg = dut.user_project.ans_block.encoder.out.value
-#         # assert state_reg == (state)  # out_vld = 1
-#         print(d, (int(state_reg), int(output_reg)), (model.state, out))
-
-#         dut.uio_in.value = 0b1001 # mode = encode, in_vld = 0, out_rdy = 1
-#         await ClockCycles(dut.clk, 2)
-#         assert dut.uio_out[4] == 1 # in_rdy = 1
-#         assert dut.uio_out[5] == 0 # out_vld = 0
-       
-
-#     assert data_in == data_out
